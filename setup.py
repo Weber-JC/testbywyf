a3a0d87d926f61fee2fc78534d22e1f43f8b68f6
weber.juche@gmail.com <weber.juche@gmail.com>
2016-05-26 18:41:39 +0800

    no message

---
+++ setup.py
@@ -0,0 +1,23 @@
+#!/usr/local/bin/python
+# -*- coding:utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name = "test",
+    packages = ['test'], # this must be the same as the name above
+    version = "0.1",    
+    zip_safe = False,
+
+    description = "test",
+    long_description = "Public Funcs",
+    author = "Weber Juche",
+    author_email = "weber.juche@gmail.com",
+
+    license = "MIT",
+    keywords = ["test", "egg"],
+    classifiers = [],
+    platforms = "Independant",
+    url = "http://pwd0.com/",
+    
+)
